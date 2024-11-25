a0ffc39a28221b60170c835aa63602e4ccc8ad82
thenuboflegend <thenuboflegend@proton.me>
2024-11-25 11:16:28 -0600

    work on functionality

--- packages.sh
+++ packages.sh
@@ -1,44 +1,25 @@
 #!/bin/bash
 
 # Define the path to the list of packages
-vital_package_list_dir="$(dirname "${BASH_SOURCE[0]}")/vital_packages.list"
-package_list_dir="$(dirname "${BASH_SOURCE[0]}")/packages.list"
+vital_package_list_dir="$PWD/vital_packages.list"
+package_list_dir="$PWD/packages.list"
 package_list=()
+script_list=()
 
 # Read the desired packages from the list file
-while IFS= read -r line; do
+while read -r line; do
     # Skip lines starting with a comment character (#)
-    if [[ "$line" =~ ^#.*$ ]] || [ -z "$line" ]; then
-        continue
+    if [[ "$line" =~ ^/.*$ ]] && [ ! -z "$line" ]; then
+        script_list+=($line)
+    elif [[ ! "$line" =~ ^#.*$ ]] && [ ! -z "$line" ]; then
+        package_list+=($line)
     fi
-    package_list+=($line)
-done < "$package_list_dir"
-
-while IFS= read -r line; do
-    # Skip lines starting with a comment character (#)
-    if [[ "$line" =~ ^#.*$ ]] || [ -z "$line" ]; then
-        continue
-    fi
-    package_list+=($line)
-done < "$vital_package_list_dir"
-
-# Get the list of explicitly installed packages
-installed_packages=""
-while IFS= read -r line; do
-    installed_packages+=" $line "
-done < <( pacman -Qqe )
-
-needed_packages=""
-for package in "${package_list[@]}"; do
-    # Check if the package is not installed
-    if [[ ! " ${installed_packages} " =~ " ${package} " ]]; then
-        needed_packages+="$package "
-        echo "Installing $package"
-    fi
-done
+done < <(cat $package_list_dir && cat $vital_package_list_dir)
 
 echo "Installing needed: "
-sudo pacman --noconfirm -S $needed_packages
+echo ${package_list[@]}
+echo ${script_list[@]}
+# sudo pacman --noconfirm --needed -S $package_list
 # Remove packages not listed in the desired list
 #for package in $installed_packages; do
 #    if ! grep -q "^$package$" "$package_list"; then
@@ -49,5 +30,5 @@ sudo pacman --noconfirm -S $needed_packages
 
 # Update all installed packages
 echo "Updating all installed packages..."
-sudo pacman -Syu --noconfirm
+# sudo pacman -Syu --noconfirm
 
